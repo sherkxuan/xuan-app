15f5a4aacb79faf61b496c74afaf9ba643eb04e1
sherk <sherkxuan@163.com>
2021-06-02 23:53:19 +0800

    修改token验证鉴权，完善异常抛出

---
+++ app/api/middleware/CheckToken.php
@@ -0,0 +1,44 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\api\middleware;
+
+use Closure;
+use Exception;
+use thans\jwt\exception\JWTException;
+use thans\jwt\exception\TokenBlacklistException;
+use thans\jwt\exception\TokenBlacklistGracePeriodException;
+use thans\jwt\JWTAuth as Auth;
+use thans\jwt\middleware\JWTAuth;
+
+class CheckToken extends JWTAuth
+{
+
+    public function __construct(Auth $auth)
+    {
+        parent::__construct($auth);
+    }
+
+    /**
+     * @param $request
+     * @param Closure $next
+     * @return mixed
+     * @throws Exception
+     */
+    public function handle($request, Closure $next)
+    {
+
+
+        try {
+            $this->auth->auth();
+        } catch (TokenBlacklistException $e) {
+            throw new Exception($e->getMessage(),211);
+        } catch (TokenBlacklistGracePeriodException $e) {
+            throw new Exception($e->getMessage(),210);
+        } catch (JWTException $e) {
+            throw new Exception($e->getMessage(),201);
+        }
+        $request->uid = ($this->auth->auth())['uid']->getValue();
+        return $next($request);
+    }
+}
